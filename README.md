3fa6ec28d7b25eaf177befa16dcbd9d468f5059a
Amfix <102174334+MrAmfix@users.noreply.github.com>
2023-10-13 01:30:50 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cs-102-template
---
+++ tests/lab1/test_calculator.py
@@ -0,0 +1,7 @@
+import unittest
+
+class CalculatorTestCase(unittest.TestCase):
+
+    # Тест для проверки работы, можно удалить
+    def test_one(self):
+        self.assertEquals(1, 1)
