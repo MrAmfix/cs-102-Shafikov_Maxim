8f1d3d9c59f365b929dfd7488d3a8aa8da6878ad
natru <nat.rud.ufa@gmail.com>
2023-10-13 19:31:46 +0300

    test+calc

---
+++ src/lab1/calculator.py
@@ -0,0 +1,67 @@
+
+def is_correct_string(strx):
+    strx = strx.lstrip()
+    if strx[0] == "+" or strx[0] == "-":
+        strx = strx[1:]
+    strx += " "
+    math_sign = ["+", "-", "*", "/"]
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    arr_check = []
+    curr_str = ""
+    for i in strx:
+        if i in math_sign or i == " ":
+            if len(curr_str) != 0:
+                if curr_str[-1] == ".":
+                    return False
+                else:
+                    arr_check.append(curr_str)
+                    curr_str = ""
+            if i != " ":
+                arr_check.append(i)
+        elif i == ".":
+            if len(curr_str) == 0:
+                return False
+            elif "." in curr_str:
+                return False
+            else:
+                curr_str += i
+        elif i in numbers:
+            curr_str += i
+        else:
+            return False
+
+    if (arr_check[0] in math_sign and (arr_check[0] != "-" or arr_check[0] != "+")) or (arr_check[-1] in math_sign):
+        return False
+    else:
+        curr_state = ""
+        if arr_check[0] in math_sign:
+            curr_state = "SIGN"
+        else:
+            curr_state = "NUMBER"
+        last_sign = ""
+        for i in arr_check[1:]:
+            if i in math_sign:
+                if curr_state == "SIGN":
+                    return False
+                else:
+                    last_sign = i
+                    curr_state = "SIGN"
+            else:
+                if curr_state == "NUMBER":
+                    return False
+                else:
+                    if last_sign == "/" and float(i) == 0:
+                        return False
+                    else:
+                        curr_state = "NUMBER"
+        return True
+
+
+def calculator(string):
+    if is_correct_string(string):
+        try:
+            return eval(string)
+        except Exception as e:
+            return "EVAL ERROR!"
+    else:
+        return "Incorrect expression"
--- tests/lab1/test_calculator.py
+++ tests/lab1/test_calculator.py
@@ -1,7 +1,13 @@
 import unittest
-
+from src.lab1.calculator import calculator
 class CalculatorTestCase(unittest.TestCase):
+    def test_calculator(self):
+        self.assertAlmostEqual(calculator("4+5"), 9)
+        self.assertAlmostEqual(calculator("-5.0  - 2.2"), -7.2)
+        self.assertEqual(calculator("5.2/ 0"), "Incorrect expression")
+        self.assertEqual(calculator("++5.2 - 44.1"), "Incorrect expression")
+        self.assertEqual(calculator("154  / 0.00000"), "Incorrect expression")
+        self.assertAlmostEqual(calculator("50.25 / 5.0 +2"), 12.05)
 
-    # Тест для проверки работы, можно удалить
-    def test_one(self):
-        self.assertEquals(1, 1)
+if __name__ == "__main__":
+    unittest.main()
