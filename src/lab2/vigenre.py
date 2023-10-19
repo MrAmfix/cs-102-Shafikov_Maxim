bc817cd6b65c8adbe5f13ea932f05cf86a4977a7
MrAmfix <nat.rud.ufa@gmail.com>
2023-10-20 01:10:00 +0300

    Реализованы функции encrypt_vigenere() и decrypt_vigenere()

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,3 +1,4 @@
+from math import ceil
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,7 +10,20 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword * ceil(len(plaintext) / len(keyword))
+    bias = []
+    bias_index = 0
+    for i in keyword.lower():
+        bias.append(ord(i) - ord('a'))
+    for i in range(0, len(plaintext)):
+        if ('a' <= plaintext[i] <= 'z') or ('A' <= plaintext[i] <= 'Z'):
+            if (plaintext[i] <= 'Z' < chr(ord(plaintext[i]) + bias[bias_index])) or (chr(ord(plaintext[i]) + bias[bias_index]) > 'z'):
+                ciphertext += chr(ord(plaintext[i]) - 26 + bias[bias_index])
+            else:
+                ciphertext += chr(ord(plaintext[i]) + bias[bias_index])
+            bias_index += 1
+        else:
+            ciphertext += plaintext[i]
     return ciphertext
 
 
@@ -24,5 +38,18 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    keyword = keyword * ceil(len(ciphertext) / len(keyword))
+    bias = []
+    bias_index = 0
+    for i in keyword.lower():
+        bias.append(ord('a') - ord(i))
+    for i in range(0, len(ciphertext)):
+        if ('A' <= ciphertext[i] <= 'Z') or ('a' <= ciphertext[i] <= 'z'):
+            if (ciphertext[i] <= 'Z' and chr(ord(ciphertext[i]) + bias[bias_index]) < 'A') or (ciphertext[i] >= 'a' > chr(ord(ciphertext[i]) + bias[bias_index])):
+                plaintext += chr(ord(ciphertext[i]) + 26 + bias[bias_index])
+            else:
+                plaintext += chr(ord(ciphertext[i]) + bias[bias_index])
+            bias_index += 1
+        else:
+            plaintext += ciphertext[i]
     return plaintext
